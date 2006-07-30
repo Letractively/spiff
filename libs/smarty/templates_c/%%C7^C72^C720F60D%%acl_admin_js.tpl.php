12a0c3fe6f71ca49936e3dce05c8159c37d011ac
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2006-07-30 16:54:31 +0000

    * Created installation instructions. * Removed precompiled smarty templates from version management. -Samuel

--- libs/smarty/templates_c/%%C7^C72^C720F60D%%acl_admin_js.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.9, created on 2006-07-23 16:36:41
-         compiled from phpgacl/acl_admin_js.tpl */ ?>
-<?php echo '
-<script language="JavaScript" src="admin_functions.js">
-</script>
-'; ?>
