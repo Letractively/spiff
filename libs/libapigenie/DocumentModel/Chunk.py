92a269ca288348b74ea0ef0353e312b4ec4bc1b9
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2006-09-29 18:29:22 +0000

    * Inserted GPL copyright header into Python files. -Samuel

--- libs/libapigenie/DocumentModel/Chunk.py
+++ libs/libapigenie/DocumentModel/Chunk.py
@@ -1,4 +1,17 @@
-# A piece of a file.
+# Copyright (C) 2006 Samuel Abels, http://debain.org
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 class Chunk:
     def __init__(self, type, data, name = ''):
         self.id          = -1
