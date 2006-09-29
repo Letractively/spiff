472b75b273d46fedc53e5548fb5b420a64889e14
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2006-09-29 11:52:36 +0000

    * libapigenie: Python parser works, document model can be   imported, and source can also be reproduced. * libapigenie: It is now possible to use a generator to   re-create API documention. This lets you easyly convert   between different API doc syntaxes. -Samuel

--- libs/libapigenie/DocumentModel/ApiDoc.py
+++ libs/libapigenie/DocumentModel/ApiDoc.py
@@ -1,27 +1,35 @@
 from Chunk import Chunk
 
 class ApiDoc(Chunk):
-    def __init__(self, data):
+    def __init__(self, data, generator = None):
         Chunk.__init__(self, 'api_doc', data, '')
         self.introduction = ''
         self.description  = ''
         self.arguments    = []
         self.argument_map = {}
         self.return_var   = None
+        self.generator    = generator
+        self.modified     = False
+
+    def attach_generator(self, generator):
+        self.generator = generator
 
     def set_introduction(self, introduction):
+        self.modified = True
         self.introduction = introduction
 
     def get_introduction(self):
         return self.introduction
 
     def set_description(self, description):
+        self.modified = True
         self.description = description
 
     def get_description(self):
         return self.description
 
     def add_argument(self, arg):
+        self.modified = True
         self.arguments.append(arg)
         self.argument_map[arg.get_name()] = arg
 
@@ -34,7 +42,21 @@ class ApiDoc(Chunk):
         return self.arguments
 
     def set_return(self, var):
+        self.modified = True
         self.return_var = var
 
     def get_return(self):
         return self.return_var
+
+    def add_child(self, child):
+        assert False # This is a stub.
+
+    def get_data(self):
+        if not self.modified:
+            return self.data
+        if self.generator is None:
+            return self.data
+        return self.generator.get_data(self)
+        
+    def mark_unmodified(self):
+        self.modified = False
