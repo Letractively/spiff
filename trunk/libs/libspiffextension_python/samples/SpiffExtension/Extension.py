34ec296888327edb9a3cbe2bf3a996b7c2afd7ea
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2006-12-13 02:20:12 +0000

    * libspiffextension_python: Installation and uninstallation of extensions   completed. * libspiffextension_python: TODO updated. -Samuel

--- trunk/libs/libspiffextension_python/samples/SpiffExtension/Extension.py
+++ trunk/libs/libspiffextension_python/samples/SpiffExtension/Extension.py
@@ -1,12 +1,12 @@
 """
 extension:    SpiffExtension
-handle:       spiff_extension
+handle:       spiff
 version:      1.1
 author:       Samuel Abels
 author-email: spam2@debain.org
 description:  A simple extension serving as a dependency for the tests.
 """
 
-class SpiffExtension:
-   def __init__(self):
+class Extension:
+   def __init__(self, api):
       pass
