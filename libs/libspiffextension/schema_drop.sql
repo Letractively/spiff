94c9ee35b1fd96251f80669d68bc65943d3d2598
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2006-12-14 18:45:56 +0000

    * Deleted all PHP libraries. * Lots of namespace cleanup (1st step, second step follows soon). -Samuel

--- libs/libspiffextension/schema_drop.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE extension_dependency;
-DROP TABLE extension_dependency_map;
-;
--- libs/libuseful/cookie.inc.php
+++
@@ -1,9 +0,0 @@
-<?
-function set_cookie($_name, $_value) {
-  if ($_COOKIE[$_name] == $_value)
-    return FALSE;
-  setcookie($_name, $_value);
-  $_COOKIE[$_name] = $_value;
-  return TRUE;
-}
-?>
