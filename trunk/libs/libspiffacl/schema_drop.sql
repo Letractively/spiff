7a678c839eec05da0ff63749e4c5502722226d3d
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2006-12-14 18:45:56 +0000

    * Deleted all PHP libraries. * Lots of namespace cleanup (1st step, second step follows soon). -Samuel

--- trunk/libs/libspiffacl/schema_drop.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE acl;
-DROP TABLE path_ancestor_map;
-DROP TABLE resource_path;
-DROP TABLE resource_attribute;
-DROP TABLE resource;
-DROP TABLE action;
-DROP TABLE resource_section;
-DROP TABLE action_section;
-;
