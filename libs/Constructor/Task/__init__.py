4040689f8ef4a24448b0616f9dda127282396785
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2006-12-27 10:02:22 +0000

    * Constructor: Added several new tasks. * Constructor: Removed unneeded code from the sample. -Samuel

--- libs/Constructor/Task/__init__.py
+++ libs/Constructor/Task/__init__.py
@@ -1,15 +1,23 @@
 __all__ = ['Task',
+           'DirExists',
+           'FileExists',
+           'FileIsWritable',
            'Group',
            'CheckPythonVersion',
+           'CheckDBConnection',
            'CollectDBInfo',
            'ExecCommand',
            'InstallationCompleted',
            'LicenseAgreement']
 
-from Task                  import *
-from Group                 import *
-from CheckPythonVersion    import *
-from CollectDBInfo         import *
-from ExecCommand           import *
-from InstallationCompleted import *
-from LicenseAgreement      import *
+from Task                      import *
+from DirExists                 import *
+from FileExists                import *
+from FileIsWritable            import *
+from Group                     import *
+from CheckPythonVersion        import *
+from CheckDBConnection         import *
+from CollectDBInfo             import *
+from ExecCommand               import *
+from InstallationCompleted     import *
+from LicenseAgreement          import *
