cda066397c45c2b2507473d3124dd1850f761a26
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2007-01-04 13:20:57 +0000

    * PHP admin_center: Removed from SVN. * Admin Center: Initial checkin. * Admin Center: Ported most Smarty templates to Genshi. -Samuel

---
+++ plugins/AdminCenter/Extension.py
@@ -0,0 +1,24 @@
+"""
+extension:    Admin Center
+handle:       spiff_core_admin_center
+version:      0.1
+author:       Samuel Abels
+author-email: spam2@debain.org
+description:  This core extension implements the admininistration user interface.
+dependency:   spiff spiff_core_login
+signal:       render_start
+              render_end
+"""
+
+class Extension:
+    def __init__(self, api):
+        self.api = api
+
+
+    def on_render_request(self):
+        self.api.emit('render_start')
+        self.api.send_headers()
+
+        #FIXME
+
+        self.api.emit('render_end')
