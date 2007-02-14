3d3b54dd28a3e0e5f42628e9043d81f8b10617d8
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2007-02-14 23:46:06 +0000

    * Spiff Guard: Added Python Cheeseshop distribution files. -Samuel

---
+++ trunk/libs/Guard/distrib.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name         = 'Spiff Guard',
+      version      = '1.0',
+      description  = 'Generic Access Lists',
+      long_description = 'Spiff Guard is a library implementing generic access lists for Python.',
+      author       = 'Samuel Abels',
+      author_email = 'cheeseshop.python.org@debain.org',
+      license      = 'GPLv2',
+      packages     = [''],
+      url          = 'http://code.google.com/p/spiff/')
