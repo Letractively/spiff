02ad5a77d6a9c76a9261eee7d7fc4bc3b7b528d1
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2007-03-10 10:35:21 +0000

    * AdminCenter: The user and group editors can now display all information   correctly. * Guard: ChangeLog added. -Samuel

--- plugins/AdminCenter/Extension.py
+++ plugins/AdminCenter/Extension.py
@@ -25,18 +25,63 @@ class Extension:
     def __show_user(self, user):
         assert user is not None
         assert not user.is_group()
-        parents = self.guard.get_resource_parents(user)
+        guard = self.guard
+
+        # Collect information for the browser.
+        parents = guard.get_resource_parents(user)
+        
+        # Collect permissions.
+        search = {'actor': user, 'resource_section_handle': 'users'}
+        acls            = guard.get_permission_list(**search)
+        acls_view       = []
+        acls_edit       = []
+        acls_delete     = []
+        acls_non_view   = []
+        acls_non_edit   = []
+        acls_non_delete = []
+        for acl in acls:
+            handle = acl.get_action().get_handle()
+            permit = acl.get_permit()
+            if permit and handle == 'view':
+                acls_view.append(acl.get_resource_id())
+            elif permit and handle == 'edit':
+                acls_edit.append(acl.get_resource_id())
+            elif permit and handle == 'delete':
+                acls_delete.append(acl.get_resource_id())
+            elif handle == 'view':
+                acls_non_view.append(acl.get_resource_id())
+            elif handle == 'edit':
+                acls_non_edit.append(acl.get_resource_id())
+            elif handle == 'delete':
+                acls_non_delete.append(acl.get_resource_id())
+
+        # Collect more permission info.
+        viewable      = guard.get_resource_list_from_id_list(acls_view)
+        editable      = guard.get_resource_list_from_id_list(acls_edit)
+        deletable     = guard.get_resource_list_from_id_list(acls_delete)
+        non_viewable  = guard.get_resource_list_from_id_list(acls_non_view)
+        non_editable  = guard.get_resource_list_from_id_list(acls_non_edit)
+        non_deletable = guard.get_resource_list_from_id_list(acls_non_delete)
+
+        # Render the template.
         self.api.render('templates/user_editor.tmpl',
-                        user   = user,
-                        groups = parents)
+                        user                 = user,
+                        groups               = parents,
+                        viewable_actors      = viewable,
+                        editable_actors      = editable,
+                        deletable_actors     = deletable,
+                        non_viewable_actors  = non_viewable,
+                        non_editable_actors  = non_editable,
+                        non_deletable_actors = non_deletable)
 
 
     def __show_group(self, group):
         assert group is not None
         assert group.is_group()
+        guard = self.guard
 
         # Collect information for the browser.
-        children = self.guard.get_resource_children(group)
+        children = guard.get_resource_children(group)
         users    = []
         groups   = []
         for child in children:
@@ -47,32 +92,48 @@ class Extension:
 
         # Collect permissions.
         search = {'actor': group, 'resource_section_handle': 'users'}
-        acls        = self.guard.get_permission_list(**search)
-        acls_view   = []
-        acls_edit   = []
-        acls_delete = []
+        acls            = guard.get_permission_list(**search)
+        acls_view       = []
+        acls_edit       = []
+        acls_delete     = []
+        acls_non_view   = []
+        acls_non_edit   = []
+        acls_non_delete = []
         for acl in acls:
             handle = acl.get_action().get_handle()
-            if handle == 'view':
+            permit = acl.get_permit()
+            if permit and handle == 'view':
                 acls_view.append(acl.get_resource_id())
-            elif handle == 'edit':
+            elif permit and handle == 'edit':
                 acls_edit.append(acl.get_resource_id())
-            elif handle == 'delete':
+            elif permit and handle == 'delete':
                 acls_delete.append(acl.get_resource_id())
+            elif handle == 'view':
+                acls_non_view.append(acl.get_resource_id())
+            elif handle == 'edit':
+                acls_non_edit.append(acl.get_resource_id())
+            elif handle == 'delete':
+                acls_non_delete.append(acl.get_resource_id())
 
         # Collect more permission info.
-        viewable  = self.guard.get_resource_list_from_id_list(acls_view)
-        editable  = self.guard.get_resource_list_from_id_list(acls_edit)
-        deletable = self.guard.get_resource_list_from_id_list(acls_delete)
+        viewable      = guard.get_resource_list_from_id_list(acls_view)
+        editable      = guard.get_resource_list_from_id_list(acls_edit)
+        deletable     = guard.get_resource_list_from_id_list(acls_delete)
+        non_viewable  = guard.get_resource_list_from_id_list(acls_non_view)
+        non_editable  = guard.get_resource_list_from_id_list(acls_non_edit)
+        non_deletable = guard.get_resource_list_from_id_list(acls_non_delete)
 
         # Render the template.
         self.api.render('templates/group_editor.tmpl',
-                        group            = group,
-                        users            = users,
-                        groups           = groups,
-                        viewable_actors  = viewable,
-                        editable_actors  = editable,
-                        deletable_actors = deletable)
+                        group                = group,
+                        users                = users,
+                        groups               = groups,
+                        viewable_actors      = viewable,
+                        editable_actors      = editable,
+                        deletable_actors     = deletable,
+                        non_viewable_actors  = non_viewable,
+                        non_editable_actors  = non_editable,
+                        non_deletable_actors = non_deletable)
 
 
     def __user_editor(self):
