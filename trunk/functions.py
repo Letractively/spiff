5aa774df4e69b8eb91e206ad57a29b67186f928d
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2007-05-18 19:46:09 +0000

    * Revert: Accidentially comitted mod_rewrite activated files. -Samuel

--- trunk/functions.py
+++ trunk/functions.py
@@ -13,7 +13,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 import os, cgi
-use_mod_rewrite = True
+use_mod_rewrite = False
 
 
 def get_uri(page, *args, **kwargs):
