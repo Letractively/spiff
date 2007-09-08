f5fbf3aaee423a30e0b9a477ef28a3f837b3262e
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2007-09-08 19:53:48 +0000

    * Integrator: Removed pointless return codes by exceptions. -Samuel

--- libs/run_tests.sh
+++ libs/run_tests.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-find . -mindepth 2 -name "run_tests.sh" | while read i; do
-  echo $i
-  cd `dirname $i`
-  sh `basename $i`
-  cd - >/dev/null
-done
-find . -mindepth 2 -name "run_suite.py" | while read i; do
+find . -name "run_suite.py" | while read i; do
   echo $i
   cd `dirname $i`
   python `basename $i`
