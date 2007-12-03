d4f845562148c8a5696609c8bc029a3450095f26
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2007-12-03 22:15:59 +0000

    * Guard: Added a script for generating the API documentation. -Samuel

---
+++ libs/Guard/mkdoc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Generates the APPI documentation.
+import os, re, glob, sys
+
+doc_file = 'Spiff_Guard.py'
+doc_dir  = 'doc'
+files = ['DBReader.py',
+         'DB.py',
+         'DBObject.py',
+         'ResourcePath.py',
+         'Acl.py',
+         'Action.py',
+         'Resource.py',
+         'ResourceGroup.py']  # Order matters - can't resolve inheritance otherwise.
+classes = [os.path.splitext(file)[0] for file in files]
+
+# Concatenate the content of all files into one file.
+remove_re = re.compile(r'^from (' + '|'.join(classes) + r') * import .*')
+fp_out    = open(doc_file, 'w')
+for file in files:
+    fp_in = open(file, 'r')
+    fp_out.write(fp_in.read())
+    fp_in.close()
+fp_out.close()
+
+os.system('epydoc ' + ' '.join(['--html',
+                                '--parse-only',
+                                '--no-private',
+                                '--no-source',
+                                '--no-frames',
+                                '--inheritance=grouped',
+                                '-v',
+                                '-o %s' % doc_dir, doc_file]))
+
+os.remove(doc_file)
