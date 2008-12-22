b99d2ab42e14cb89131d8fd4714979775bc2f4fb
knipknap <knipknap@be0c91e1-b319-0410-904a-213b553f791a>
2008-12-22 21:56:00 +0000

    Get rid of obsolete libraries. This leaves the repository in a broken state.

--- libs/Integrator/samples/SpiffExtension/Extension.py
+++
@@ -1,9 +0,0 @@
-class Extension:
-    def __init__(self, api):
-        pass
-
-    def on_spiff_page_open(self, **args):
-        pass
-
-    def on_spiff_header_send_before(self, **args):
-        pass
--- libs/Workflow/src/Workflow/__init__.py
+++
@@ -1,8 +0,0 @@
-from Job        import Job
-from Workflow   import Workflow
-from Exception  import WorkflowException
-from BranchNode import BranchNode
-
-import inspect
-__all__ = [name for name, obj in locals().items()
-           if not (name.startswith('_') or inspect.ismodule(obj))]
